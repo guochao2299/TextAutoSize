59a5f92f7ae7619b5451ba184b730fd741d346e3
guochao2299 <szf.0325@gmail.com>
2018-03-27 22:00:20 +0800

    Add files via upload

---
+++ AutoSizeFont/AutoSizeFont/frmMain.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace AutoSizeFont
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rbImageZoom = new System.Windows.Forms.RadioButton();
+            this.rbAutoFontSize = new System.Windows.Forms.RadioButton();
+            this.rbNormal = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtDesc = new System.Windows.Forms.RichTextBox();
+            this.pnlBoard = new System.Windows.Forms.Panel();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.btnDraw);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.txtContent);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pnlBoard);
+            this.splitContainer1.Size = new System.Drawing.Size(434, 210);
+            this.splitContainer1.SplitterDistance = 124;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(357, 77);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(68, 45);
+            this.btnDraw.TabIndex = 4;
+            this.btnDraw.Text = "绘  制";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rbImageZoom);
+            this.groupBox2.Controls.Add(this.rbAutoFontSize);
+            this.groupBox2.Controls.Add(this.rbNormal);
+            this.groupBox2.Location = new System.Drawing.Point(12, 76);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(339, 45);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "绘制方法";
+            // 
+            // rbImageZoom
+            // 
+            this.rbImageZoom.AutoSize = true;
+            this.rbImageZoom.Location = new System.Drawing.Point(241, 20);
+            this.rbImageZoom.Name = "rbImageZoom";
+            this.rbImageZoom.Size = new System.Drawing.Size(71, 16);
+            this.rbImageZoom.TabIndex = 2;
+            this.rbImageZoom.Text = "图片缩放";
+            this.rbImageZoom.UseVisualStyleBackColor = true;
+            // 
+            // rbAutoFontSize
+            // 
+            this.rbAutoFontSize.AutoSize = true;
+            this.rbAutoFontSize.Location = new System.Drawing.Point(104, 20);
+            this.rbAutoFontSize.Name = "rbAutoFontSize";
+            this.rbAutoFontSize.Size = new System.Drawing.Size(119, 16);
+            this.rbAutoFontSize.TabIndex = 1;
+            this.rbAutoFontSize.Text = "自动调整字体大小";
+            this.rbAutoFontSize.UseVisualStyleBackColor = true;
+            // 
+            // rbNormal
+            // 
+            this.rbNormal.AutoSize = true;
+            this.rbNormal.Checked = true;
+            this.rbNormal.Location = new System.Drawing.Point(16, 20);
+            this.rbNormal.Name = "rbNormal";
+            this.rbNormal.Size = new System.Drawing.Size(71, 16);
+            this.rbNormal.TabIndex = 0;
+            this.rbNormal.TabStop = true;
+            this.rbNormal.Text = "正常绘制";
+            this.rbNormal.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(230, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "文本内容：";
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(218, 29);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(207, 41);
+            this.txtContent.TabIndex = 1;
+            this.txtContent.Text = "变形金刚";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtDesc);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 58);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "基本情况";
+            // 
+            // txtDesc
+            // 
+            this.txtDesc.BackColor = System.Drawing.Color.LightCyan;
+            this.txtDesc.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtDesc.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtDesc.Location = new System.Drawing.Point(3, 17);
+            this.txtDesc.Name = "txtDesc";
+            this.txtDesc.Size = new System.Drawing.Size(194, 38);
+            this.txtDesc.TabIndex = 0;
+            this.txtDesc.Text = "文本框大小：宽100，高25\n字体：尺寸15，总体";
+            // 
+            // pnlBoard
+            // 
+            this.pnlBoard.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pnlBoard.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlBoard.Location = new System.Drawing.Point(0, 0);
+            this.pnlBoard.Name = "pnlBoard";
+            this.pnlBoard.Size = new System.Drawing.Size(434, 82);
+            this.pnlBoard.TabIndex = 0;
+            this.pnlBoard.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlBoard_Paint);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 210);
+            this.Controls.Add(this.splitContainer1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "自动调整文本大小";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel pnlBoard;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox txtDesc;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rbNormal;
+        private System.Windows.Forms.RadioButton rbAutoFontSize;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.RadioButton rbImageZoom;
+    }
+}
+
---
+++ AutoSizeFont/AutoSizeFont/frmMain.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AutoSizeFont
+{
+    public partial class frmMain : Form
+    {
+        private Rectangle m_rect = new Rectangle(150, 20, 100, 25);
+        private Bitmap m_image = null;
+        private const int ORIGINAL_FONT_SIZE=15;
+        private const string FONT_NAME = "宋体";
+        private const float FONT_SIZE_CHANGE_STEP = 0.5f;
+        private string m_content = string.Empty;
+
+        private delegate void DrawTextDelegate(Graphics g,string content);
+        private DrawTextDelegate m_textDrawer = null;
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            this.DoubleBuffered = true;
+        }
+
+        private void pnlBoard_Paint(object sender, PaintEventArgs e)
+        {
+            using (Pen p = new Pen(Color.Red, 1))
+            {
+                e.Graphics.DrawRectangle(p, m_rect);
+            }
+
+            if (m_textDrawer == null)
+            {
+                return;
+            }
+
+            m_textDrawer(e.Graphics, m_content);
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtContent.Text))
+            {
+                MessageBox.Show("绘制的内容不能为空");
+                txtContent.Focus();
+                return;
+            }
+
+            m_content=txtContent.Text;
+
+            if (rbNormal.Checked)
+            {
+                m_textDrawer = DrawTextNormal;
+            }
+            else if (rbAutoFontSize.Checked)
+            {
+                m_textDrawer = DrawTextAutoChangeFontSize;
+            }
+            else
+            {
+                using (Graphics g = pnlBoard.CreateGraphics())
+                {
+                    using (Font f = new Font(FONT_NAME, ORIGINAL_FONT_SIZE))
+                    {
+                        SizeF contentSize = g.MeasureString(m_content, f);
+                        if (m_image != null)
+                        {
+                            m_image.Dispose();
+                        }
+                        m_image = new Bitmap(Convert.ToInt32(contentSize.Width + 1), Convert.ToInt32(contentSize.Height + 1));
+
+                        using (Graphics gImage = Graphics.FromImage(m_image))
+                        {
+                            gImage.DrawString(m_content, f, Brushes.Blue, new Rectangle(0, 0, m_image.Width, m_image.Height));
+                        }
+                    }
+                }
+                    
+                m_textDrawer = DrawTextZoomImage;
+            }
+
+            pnlBoard.Refresh();
+        }
+
+        private void DrawTextNormal(Graphics g, string content)
+        {
+            using (Font f = new Font(FONT_NAME, ORIGINAL_FONT_SIZE))
+            {
+                g.DrawString(content, f, Brushes.Blue, m_rect);
+            }
+        }
+
+        private void DrawTextAutoChangeFontSize(Graphics g, string content)
+        {
+            float fontSize = ORIGINAL_FONT_SIZE;
+            Font f = new Font(FONT_NAME, ORIGINAL_FONT_SIZE);
+
+            float contentWidth = g.MeasureString(content, f).Width;
+            while ((contentWidth > m_rect.Width))
+            {
+                f.Dispose();
+                fontSize-=FONT_SIZE_CHANGE_STEP;
+
+                f = new Font(FONT_NAME, fontSize);
+                contentWidth = g.MeasureString(content, f).Width;
+            }
+
+            g.DrawString(content, f, Brushes.Blue, m_rect);
+            f.Dispose();            
+        }
+
+        private void DrawTextZoomImage(Graphics g, string content)
+        {
+            g.DrawImage(m_image, m_rect);
+        }
+
+    }
+}
